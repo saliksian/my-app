658a5eabeeb648114a0667c283dcf7b31bd31da4
saliksian <salikafzal91@gmail.com>
2022-01-14 20:22:04 +0500

    Update index.js
    navbar open by clicking the hamburger icon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ".//style.css";
+import 'bootstrap';
 
 ReactDOM.render(
   <React.StrictMode>
