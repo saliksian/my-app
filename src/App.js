21d71914f1ddf017b9cf887265b8156d11918cdb
saliksian <salikafzal91@gmail.com>
2022-01-16 20:07:35 +0500

    updated
    add remaining groups

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Group1 from "./Components/Group1.jsx";
 import Group2 from "./Components/Group2.jsx";
 import Group3 from "./Components/Group3.jsx";
 import Group4 from "./Components/Group4";
+import Group5 from "./Components/Group5";
+import Group6 from "./Components/Group6";
+import Group7 from "./Components/Group7";
+import Group8 from "./Components/Group8";
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Group2 />
       <Group3 />
       <Group4 />
+      <Group5 />
+      <Group6 />
+      <Group7 />
+      <Group8 />
     </>
   );
 }
